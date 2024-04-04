7e6e6e09da5b05c3141932a085d9a06efd26490f
Jahonsher <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>
2024-04-04 23:40:02 +0500

    as

---
+++ index.js
@@ -0,0 +1,41 @@
+// 1-masala
+
+// function replaceDots(input) {
+//     return input.replace(/\./g, '[.]');
+// }
+
+// var input = '1.1.1;';
+// var output = replaceDots(input);
+// console.log(output);
+
+
+// 2-masala
+
+// let array = ["home", "joke", "asomiddin"];
+// let lastElementLength = array[array.length - 1].length;
+// console.log(lastElementLength);
+
+
+
+// 3-masala
+
+
+// let obj = {
+//     1: "home",
+//     2: "joke",
+// };
+
+// let keyCount = Object.keys(obj).length;
+// console.log(keyCount);
+
+
+
+// 4-masala
+
+// n5 logarifm
+// function factorial(n) {
+//     return n != 1 ? n * factorial(n - 1) : 1;
+//   }
+  
+//   console.log(factorial(5));
+
